992f49aeb26cc4541be0987ad5579366e44a4461
alan.souza <alancesarsouza@gmail.com>
2020-05-19 14:40:45 -0300

    Cria Todo com CASL

---
+++ src/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => (
+  <>
+    <form>
+      <div>
+      <label htmlFor="email">Email</label>
+      <input type="email"/>
+    </div> 
+
+    <div>
+      <label htmlFor="password">Senha</label>
+      <input type="password"/>
+      </div>
+    </form>
+  </>
+)
+
+export default Login
---
+++ src/config/ability.js
@@ -0,0 +1,19 @@
+import { AbilityBuilder } from '@casl/ability'
+
+/**
+ * Defines how to detect object's type: https://stalniy.github.io/casl/abilities/2017/07/20/define-abilities.html
+ */
+
+function subjectName(item) {
+  if (!item || typeof item === 'string') {
+    return item
+  }
+
+  return item.__type
+}
+
+const Ability = AbilityBuilder.define({ subjectName }, can => {
+  can(['read', 'create'], 'Todo')
+  can(['update', 'delete'], 'Todo', { assignee: 'me' })
+})
+export default Ability
